4289c86cafd3afff670b7e8903d15938219ac0bd
franklin <igboekwulusifranklin@gmail.com>
2022-03-16 00:35:08 +0100

    corrected an error in task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,7 +6,7 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _putchar(char c)
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
@@ -17,4 +17,5 @@ int _putchar(char c)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+	return (0);
 }
