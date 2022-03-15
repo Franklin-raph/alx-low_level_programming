beaa36a8cd8a94c2b71fe147e86a4d7616f97ea0
franklin <igboekwulusifranklin@gmail.com>
2022-03-16 00:58:30 +0100

    code to print all the natural numbers up to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_to_98 - check the code.
+ *
+ * @n: the parameter to start counting from
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+	int end;
+
+	end = 98;
+	while(n <= end)
+	{
+		return (n);
+	}
+	return (0);
+}
