9edd6e8e966ffe57b1282089ecf8500f0454fc84
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-03-17 13:20:11 +0100

    code to print out numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_numbers: Print all numbers from 0 to 9
+ * 
+ * Returns all numbers from 0 to 9
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	num = 48;
+	while (num <= 57)
+	{
+		_putchar(num);
+		num++;
+	}
+	_putchar('\n');
+}
