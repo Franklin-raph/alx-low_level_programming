ea2a06f6ad48d02c5be24115f9fba7effe682ae0
Franklin-raph <igboekwulusifranklin@gmail.com>
2022-03-17 12:32:15 +0100

    fixed some errors

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,9 +6,9 @@
  * Return: 1 if its upper else returns 0
  */
 
-int _isupper(int c)
+int _isupper(int C)
 {
-	if (c >= 'A' && c <= 'Z')
+	if (C >= 'A' && C <= 'Z')
 		return (1);
 	else
 		return (0);
